a17c192ee840ba5721c57b29b642cd2d5ebd47e2
Kevin López <kevin.lopezcala99@gmail.com>
2020-04-05 21:26:59 +0200

    :penguin:

---
+++ jquery/js/wsinvocations.js
@@ -0,0 +1,75 @@
+function getHello() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/",
+    success: function(data) {
+      $("#resGetHello").html(data);
+    },
+    error: function(res) {
+      alert("ERROR:" + res.statusText);
+    }
+  });
+}
+
+function getHello2() {
+  var req = $.ajax({ type: "GET", url: "http://localhost:8080/" });
+  req.done(function(data) {
+    $("#resGetHello").html(data);
+  });
+  req.fail(function(res) {
+    alert("ERROR" + res.statusText);
+  });
+}
+
+// mostrar info con id peli
+function getMovie(movieId) {
+  var myUrl = "http://localhost:8080/movies/" + movieId;
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: myUrl,
+    success: function(data) {
+      $("#resGetHello").html(JSON.stringify(data[0]));
+    },
+    error: function(res) {
+      alert("ERROR" + res.statusText);
+    }
+  });
+}
+
+// crear cosas con esos datos
+function postMovie() {
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/movies/",
+    contentType: "application/json",
+    dataType: "text",
+    data: JSON.stringify({
+      title: "Dunkirk",
+      director: "ChristopherNolan",
+      year: 2017
+    }),
+    success: function(data) {
+      $("#resGetHello").html(data);
+    },
+    error: function(res) {
+      alert("ERROR" + res.statusText);
+    }
+  });
+}
+
+// todas las pelis
+function getAllMovies() {
+  var myUrl = "http://localhost:8080/movies/";
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: myUrl,
+    success: function(data) {
+      $("#resGetHello").html(JSON.stringify(data));
+    },
+    error: function(res) {
+      alert("ERROR" + res.statusText);
+    }
+  });
+}
