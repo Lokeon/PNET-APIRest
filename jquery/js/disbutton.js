30ef3ea2bf1b0c5dc36fb8ef84e4d7e2d3723f0a
Kevin López <kevin.lopezcala99@gmail.com>
2020-04-08 18:49:51 +0200

    :penguin:

--- jquery/js/disbutton.js
+++ jquery/js/disbutton.js
@@ -1,16 +1,18 @@
 (function () {
-  $("#forName,#forDescription,#forMaxAsist").bind("keyup change", function () {
-    var empty = false;
-    $("#forName,#forDescription,#forMaxAsist").each(function () {
-      if ($(this).val() == "") {
-        empty = true;
+  $("input[type='text'],input[type='number']").bind(
+    "keyup change",
+    function () {
+      var empty = false;
+      $("input[type='text'],input[type='number']").each(function () {
+        if ($(this).val() == "") {
+          empty = true;
+        }
+      });
+      if (empty) {
+        $("#btnSubmit").attr("disabled", "disabled");
+      } else {
+        $("#btnSubmit").removeAttr("disabled");
       }
-    });
-
-    if (empty) {
-      $("#btnSubmit").attr("disabled", "disabled");
-    } else {
-      $("#btnSubmit").removeAttr("disabled");
     }
-  });
+  );
 })();
